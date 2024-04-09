0f4988736d5297a91ff3b4cdac313a6c5a5d71a5
guiabbehusen <guisoares1601@gmail.com>
2024-04-09 18:38:49 -0300

    Chess layer and printing the board

---
+++ src/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+import chess.ChessMatch;
+
+public class App {
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
+        
+        
+    }
+}
