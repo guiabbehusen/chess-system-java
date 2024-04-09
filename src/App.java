1952382838a4399d1fcf19827e2742722afe08c8
guiabbehusen <guisoares1601@gmail.com>
2024-04-09 17:37:11 -0300

    First Class Position

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Position pos = new Position(1,  2);
+        System.out.println(pos);
+        
     }
 }
