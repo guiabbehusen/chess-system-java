0aa426b341c53d308224ada0ecf2ab9aa461c0f9
guiabbehusen <guisoares1601@gmail.com>
2024-04-10 22:48:54 -0300

    Placing pieces on the board

--- src/application/App.java
+++ src/application/App.java
@@ -6,8 +6,8 @@ public class App {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
-
-        
-        
+    
     }
 }
+
+
